c777f1be9e574c93bef32c14676007dc75b483b0
mata <mata@LAPTOP-3F8AKDPU>
2019-11-08 00:25:57 +0530

    Test Commit.

---
+++ GitJenkinsIntegration/src/main/java/FirstTC.java
@@ -0,0 +1,17 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+	public class FirstTC {
+		
+		
+		@Test
+		public void firsttestMethod1()
+		{
+			System.out.println("inside firsttestMethod1 ");
+			Assert.assertEquals(true, true);
+		}
+		
+
+	
+
+}
